b7a2a6f8c315cb65aaa87bb12e3156fcb3cbb29c
tsege <tsege@10.0.0.3>
2022-05-21 06:04:10 -0700

    commit

---
+++ src/test/java/steps/XYZSmokeStep.java
@@ -0,0 +1,9 @@
+package steps;
+
+public class XYZSmokeStep {
+	@Given("User navigates to login page")
+	public void user_navigates_to_login_page() {
+		System.out.println("User able to login...");
+	}
+
+}
---
+++ src/test/resources/orangehrm/SmokeTest.feature
@@ -0,0 +1,3 @@
+Feature: Description of feature/functionality we will b testing
+Scenario: Verify user able to login successfuly
+Given User navigates to login page
